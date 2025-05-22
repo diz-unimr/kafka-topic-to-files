063a1e088ce3004ad9d732a0a903ff6ad8949b13
Jakub Lidke <jakub.lidke@uni-marburg.de>
2025-05-22 10:00:50 +0200

    initial commit

---
+++ src/main/java/de/unimarburg/diz/kafkatopic2files/Kafkatopic2filesApplication.java
@@ -0,0 +1,13 @@
+package de.unimarburg.diz.kafkatopic2files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafkatopic2filesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafkatopic2filesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/unimarburg/diz/kafkatopic2files/Kafkatopic2filesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unimarburg.diz.kafkatopic2files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafkatopic2filesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
